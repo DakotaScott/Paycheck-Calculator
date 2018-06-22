bee6989b58022baf68f291482ef1e102f47e16f3
Dakota Scott <contact@dakotacscott.com>
2018-06-21 20:16:28 -0400

    Finished implementing singleBiweeklyWitholding method and created generateReport method.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,10 +10,10 @@ public class Main {
         Scanner keyboard = new Scanner(System.in);
 
         BigDecimal schoolDistricTaxRate = new BigDecimal("0.00");
-        BigDecimal federalTaxRate = new BigDecimal("0.00");
         BigDecimal localTaxRate = new BigDecimal("0.00");
         BigDecimal MEDICARE_TAX_RATE = new BigDecimal("0.0145");
         BigDecimal stateTax = new BigDecimal("0.00");
+        BigDecimal additionalWithholdingAmt = new BigDecimal("0.00");
 
         BigDecimal grossEarnings;
         BigDecimal wagesPerHour;
@@ -21,6 +21,10 @@ public class Main {
         BigDecimal netEarnings = new BigDecimal("0.00");
         BigDecimal hoursWorked;
 
+        // Currency formatter
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        // Prompts start here
         System.out.println("PAYCHECK CALCULATOR");
         System.out.println("WARNING: CALCULATIONS ONLY APPLY TO BIWEEKLY PAY PERIODS");
         System.out.println("Proceed? (Y/N)");
@@ -35,18 +39,42 @@ public class Main {
 
         grossEarnings = wagesPerHour.multiply(hoursWorked);
 
+        System.out.println("Enter additional amount withheld for income tax: ");
+        System.out.println("If none, enter \"0\"");
+        additionalWithholdingAmt = keyboard.nextBigDecimal();
+
+        // Because tax bracket calculations only rely on gross earnings AFTER additional withholding, we calculate this here
+        BigDecimal grossEarningsDeducted = grossEarnings.subtract(additionalWithholdingAmt);
+        federalTaxWithheld = singleBiweeklyWithholding(grossEarningsDeducted);
+
+        System.out.println("Generating report...");
+        System.out.println();
+
+        // Calls report generating method
+        generateReport(grossEarnings, grossEarningsDeducted, additionalWithholdingAmt, federalTaxWithheld, hoursWorked,
+                wagesPerHour);
+    }
+
+    public static void generateReport(BigDecimal grossEarnings, BigDecimal grossEarningsDeducted, BigDecimal
+                                      additionalWithholdingAmt, BigDecimal federalTaxWithheld, BigDecimal hoursWorked,
+                                      BigDecimal wagesPerHour) {
         // Currency formatter
         DecimalFormat df = new DecimalFormat("0.00");
 
-        System.out.println("$" + df.format(grossEarnings));
-
-        federalTaxWithheld = singleBiweeklyWithholdingRate(grossEarnings);
-
-        System.out.println("$" + federalTaxWithheld);
+        System.out.println("=====REPORT=====");
+        System.out.println("Hourly Wage: $" + df.format(wagesPerHour));
+        System.out.println("Gross Earnings: $" + df.format(grossEarnings));
+        System.out.println("Additional Withholding: $" + df.format(additionalWithholdingAmt));
+        System.out.println("Federal Taxes: $" + df.format(federalTaxWithheld));
+        //Unfinished
+        System.out.println("State Tax: $");
+        System.out.println("Local Tax: $");
+        System.out.println("School District Taxes: $");
+        System.out.println("Medicare: $");
+        System.out.println("Net Earnings: $");
     }
 
-    public static char checkStatus()
-    {
+    public static char checkStatus() {
         // Keyboard input initialization
         Scanner keyboard = new Scanner(System.in);
 
@@ -68,21 +96,129 @@ public class Main {
         return choice;
     }
 
-    public static BigDecimal singleBiweeklyWithholdingRate(BigDecimal grossEarnings) {
-        BigDecimal firstBracketFloor = new BigDecimal("142.00");
-        BigDecimal firstBracketCeiling = new BigDecimal("509.00");
+    public static BigDecimal singleBiweeklyWithholding(BigDecimal grossEarningsDeducted) {
+        // First bracket
+        BigDecimal FIRST_BRACKET_FLOOR = new BigDecimal("142.00");
+        BigDecimal FIRST_BRACKET_CEILING = new BigDecimal("509.00");
+        // Second bracket
+        BigDecimal SECOND_BRACKET_FLOOR = new BigDecimal("509.00");
+        BigDecimal SECOND_BRACKET_CEILING = new BigDecimal("1631.00");
+        // Third bracket
+        BigDecimal THIRD_BRACKET_FLOOR = new BigDecimal("1631.00");
+        BigDecimal THIRD_BRACKET_CEILING = new BigDecimal("3315.00");
+        // Fourth bracket
+        BigDecimal FOURTH_BRACKET_FLOOR = new BigDecimal("3315.00");
+        BigDecimal FOURTH_BRACKET_CEILING = new BigDecimal("6200.00");
+        // Fifth bracket
+        BigDecimal FIFTH_BRACKET_FLOOR = new BigDecimal("6200.00");
+        BigDecimal FIFTH_BRACKET_CEILING = new BigDecimal("7835.00");
+        // Sixth bracket
+        BigDecimal SIXTH_BRACKET_FLOOR = new BigDecimal("7835.00");
+        BigDecimal SIXTH_BRACKET_CEILING = new BigDecimal("19373.00");
+        // Seventh bracket
+        BigDecimal SEVENTH_BRACKET_FLOOR = new BigDecimal("19373.00");
 
-        if (grossEarnings.compareTo(firstBracketFloor) > 0 && grossEarnings.compareTo(firstBracketCeiling) <= 0) {
 
-            BigDecimal bd = new BigDecimal("0.010");
-            System.out.println("First Bracket: Returned");
-            return bd;
-        } else if (grossEarnings.compareTo(secondBracketFloor) > 0 && grossEarnings.compareTo(secondBracketCeiling) <= 0) {
+        if (grossEarningsDeducted.compareTo(FIRST_BRACKET_FLOOR) > 0 && grossEarningsDeducted.compareTo(FIRST_BRACKET_CEILING) <= 0) {
 
-            BigDecimal bd = new BigDecimal("0.0120");
-            System.out.println("Second Bracket: Returned");
+            BigDecimal FIRST_BRACKET_RATE = new BigDecimal("0.10");
+            BigDecimal FIRST_BRACKET_AMT = new BigDecimal("0.00");
+
+            // Calculations
+            BigDecimal excessAmt = grossEarningsDeducted.subtract(FIRST_BRACKET_FLOOR);
+            BigDecimal rateDeduction = excessAmt.multiply(FIRST_BRACKET_RATE);
+            BigDecimal totalWithheld = FIRST_BRACKET_AMT.add(rateDeduction);
+
+            // Console output for testing purposes
+            //System.out.println("Percentage: " + rateDeduction);
+            //System.out.println("Flat amount: " + FIRST_BRACKET_AMT);
+            //System.out.println("First Bracket: Returned");
+
+            return totalWithheld;
+        } else if (grossEarningsDeducted.compareTo(SECOND_BRACKET_FLOOR) > 0 && grossEarningsDeducted.compareTo(SECOND_BRACKET_CEILING) <= 0) {
+
+            BigDecimal SECOND_BRACKET_RATE = new BigDecimal("0.12");
+            BigDecimal SECOND_BRACKET_AMT = new BigDecimal("36.70");
+
+            // Calculations
+            BigDecimal excessAmt = grossEarningsDeducted.subtract(SECOND_BRACKET_FLOOR);
+            BigDecimal rateDeduction = excessAmt.multiply(SECOND_BRACKET_RATE);
+            BigDecimal totalWithheld = SECOND_BRACKET_AMT.add(rateDeduction);
+
+            // Console output for testing purposes
+            //System.out.println("Percentage: " + rateDeduction);
+            //System.out.println("Flat amount: " + SECOND_BRACKET_AMT);
+            //System.out.println("Total: " + totalWithheld);
+            //System.out.println("Second Bracket: Returned");
+
+            return totalWithheld;
+        } else if (grossEarningsDeducted.compareTo(THIRD_BRACKET_FLOOR) > 0 && grossEarningsDeducted.compareTo(THIRD_BRACKET_CEILING) <= 0) {
+            BigDecimal THIRD_BRACKET_RATE = new BigDecimal("0.22");
+            BigDecimal THIRD_BRACKET_AMT = new BigDecimal("171.34");
+
+            // Calculations
+            BigDecimal excessAmt = grossEarningsDeducted.subtract(THIRD_BRACKET_FLOOR);
+            BigDecimal rateDeduction = excessAmt.multiply(THIRD_BRACKET_RATE);
+            BigDecimal totalWithheld = THIRD_BRACKET_AMT.add(rateDeduction);
+
+            //System.out.println("Third Bracket: Returned");
+
+            return totalWithheld;
+        } else if (grossEarningsDeducted.compareTo(FOURTH_BRACKET_FLOOR) > 0 && grossEarningsDeducted.compareTo(FOURTH_BRACKET_CEILING) <= 0) {
+
+            BigDecimal FOURTH_BRACKET_RATE = new BigDecimal("0.24");
+            BigDecimal FOURTH_BRACKET_AMT = new BigDecimal("541.82");
+
+            // Calculations
+            BigDecimal excessAmt = grossEarningsDeducted.subtract(FOURTH_BRACKET_FLOOR);
+            BigDecimal rateDeduction = excessAmt.multiply(FOURTH_BRACKET_RATE);
+            BigDecimal totalWithheld = FOURTH_BRACKET_AMT.add(rateDeduction);
+
+            //System.out.println("Fourth Bracket: Returned");
+
+            return totalWithheld;
+        } else if (grossEarningsDeducted.compareTo(FIFTH_BRACKET_FLOOR) > 0 && grossEarningsDeducted.compareTo(FIFTH_BRACKET_CEILING) <= 0) {
+            BigDecimal FIFTH_BRACKET_RATE = new BigDecimal("0.32");
+            BigDecimal FIFTH_BRACKET_AMT = new BigDecimal("1234.22");
+
+            // Calculations
+            BigDecimal excessAmt = grossEarningsDeducted.subtract(FIFTH_BRACKET_FLOOR);
+            BigDecimal rateDeduction = excessAmt.multiply(FIFTH_BRACKET_RATE);
+            BigDecimal totalWithheld = FIFTH_BRACKET_AMT.add(rateDeduction);
+
+            //System.out.println("Fifth Bracket: Returned");
+
+            return totalWithheld;
+        } else if (grossEarningsDeducted.compareTo(SIXTH_BRACKET_FLOOR) > 0 && grossEarningsDeducted.compareTo(SIXTH_BRACKET_CEILING) <= 0) {
+            BigDecimal SIXTH_BRACKET_RATE = new BigDecimal("0.35");
+            BigDecimal SIXTH_BRACKET_AMT = new BigDecimal("1757.42");
+
+            // Calculations
+            BigDecimal excessAmt = grossEarningsDeducted.subtract(SIXTH_BRACKET_FLOOR);
+            BigDecimal rateDeduction = excessAmt.multiply(SIXTH_BRACKET_RATE);
+            BigDecimal totalWithheld = SIXTH_BRACKET_AMT.add(rateDeduction);
+
+            //System.out.println("Sixth Bracket: Returned");
+
+            return totalWithheld;
+        } else if (grossEarningsDeducted.compareTo(SEVENTH_BRACKET_FLOOR) > 0) {
+            BigDecimal SEVENTH_BRACKET_RATE = new BigDecimal("0.37");
+            BigDecimal SEVENTH_BRACKET_AMT = new BigDecimal("5795.72");
+
+            // Calculations
+            BigDecimal excessAmt = grossEarningsDeducted.subtract(SEVENTH_BRACKET_FLOOR);
+            BigDecimal rateDeduction = excessAmt.multiply(SEVENTH_BRACKET_RATE);
+            BigDecimal totalWithheld = SEVENTH_BRACKET_AMT.add(rateDeduction);
+
+            //System.out.println("Seventh Bracket: Returned");
+
+            return totalWithheld;
+        } else {
+            //Not taxed
+            BigDecimal amt = new BigDecimal("0.00");
+            //System.out.println("No tax.");
+
+            return amt;
         }
-
-        return null;
     }
 }
